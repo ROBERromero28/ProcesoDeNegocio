96b289c4ab48a3e2396d38af21ee9d00718d50c7
SCIS3-PC02\SCIS3-PC02 <dandre91y_x792c@tigpe.com>
2022-03-16 19:01:24 -0500

    primer commit para subir los archivos iniciales dle proyecto

---
+++ src/main/java/com/rober/demo/PrimeraappApplication.java
@@ -0,0 +1,21 @@
+package com.rober.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PrimeraappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeraappApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value="name",defaultValue = "mundo")String name){
+		return String.format("hola %s!",name);
+	}
+}
---
+++ src/test/java/com/rober/demo/PrimeraappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rober.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeraappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
