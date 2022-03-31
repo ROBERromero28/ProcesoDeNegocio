5b0838a62f6332bfb2ba11aab8e110209a803ecc
SCIS3-PC07\SCIS3-07 <colombia@hotmail.com>
2022-03-30 19:55:48 -0500

    Configuracion user

--- src/main/java/com/rober/demo/controller/UserController.java
+++ src/main/java/com/rober/demo/controller/UserController.java
@@ -7,10 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 public class UserController {
@@ -33,13 +30,72 @@ public class UserController {
     }
 
     @RequestMapping(value = "api/users",method = RequestMethod.POST)
-    public User createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        Map<String, String> response = new LinkedHashMap<>();
+        try{
+            userRepository.save(user);
+            response.put("Success","user registered");
+            response.put("message","user registered success");
+            response.put("status",HttpStatus.OK.toString());
+            return new ResponseEntity(response,HttpStatus.OK);
+        }catch (Exception e){
+            response.put("error","Error");
+            response.put("message","No se registro a ninguno");
+            response.put("status",HttpStatus.INTERNAL_SERVER_ERROR.toString());
+            return new ResponseEntity(response,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
-        return userRepository.save(user);
 
     }
     @RequestMapping(value="api/users",method=RequestMethod.GET)
     public List<User> listUsers(){
         return userRepository.findAll();
+    }
+
+    @RequestMapping(value="api/users/{id}",method = RequestMethod.PUT)
+    public ResponseEntity<User> editUser(@RequestBody User newUser , @PathVariable Long id){
+        Map<String, String> response = new HashMap<>();
+        try{
+            User user = userRepository.findById(id).get();
+            user.setFirstName(newUser.getFirstName());
+            user.setLastName(newUser.getLastName());
+            user.setEmail(newUser.getEmail());
+            user.setPassword(newUser.getPassword());
+            response.put("Success","user edit");
+            response.put("message","user edit success");
+            response.put("status",HttpStatus.OK.toString());
+            userRepository.save(user);
+            return new ResponseEntity(response,HttpStatus.OK);
+
+        }catch (Exception e){
+            response.put("error","Not found");
+            response.put("message","User not found!");
+            response.put("status",HttpStatus.NOT_FOUND.toString());
+            return new ResponseEntity(response,HttpStatus.NOT_FOUND);
+        }
+
+
+    }
+    @RequestMapping(value="api/users/{id}",method=RequestMethod.DELETE)
+    public ResponseEntity<User> deleteUser(@PathVariable Long id){
+        Map<String, String> response = new HashMap<>();
+        try{
+            User user = userRepository.findById(id).get();
+
+            userRepository.delete(user);
+            response.put("Success","user delete");
+            response.put("message","user delete success");
+            response.put("status",HttpStatus.OK.toString());
+            return new ResponseEntity(response,HttpStatus.OK);
+        }catch (Exception e){
+            response.put("error","Not found");
+            response.put("message","User not found!");
+            response.put("status",HttpStatus.NOT_FOUND.toString());
+            return new ResponseEntity(response,HttpStatus.NOT_FOUND);
+        }
+
+
+
+
     }
 }
